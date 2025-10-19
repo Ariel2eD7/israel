1cec964f60aaa216c7e5c789f92c33986e3dc2ef
Ariel Sabag <relhightech@gmail.com>
2025-10-19 12:08:35 +0300

    d

--- wp-content/plugins/swipeable-card-deck/script.js
+++ wp-content/plugins/swipeable-card-deck/script.js
@@ -193,7 +193,7 @@ function createTheoryCard(data) {
           <div class="card-top">
           <div class="job-description category" style="color: var(--text-color);">${data.category}</div>
           </div>
-          <div class="job-position question-title" style="color: var(--text-color);">${data.question}</div>
+          <div class="job-position question-title" style="color: var(--text-color); padding-bottom: 30px;">${data.question}</div>
           
           <div class="question-answer" style="color: var(--text-color);">${sanitizedAnswer}</div>
           <button class="show-answer-btn" style="border-color: white; background-color: var(--button-bg-color) !important; color: var(--text-color);">Show Answer</button> <!-- Add this -->
