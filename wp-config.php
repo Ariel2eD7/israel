8a5fb7b0f0ae8e5660a25eac8275b2c11574ac89
Ariel Sabag <relhightech@gmail.com>
2025-10-20 17:57:47 +0300

    sac

--- wp-config.php
+++ wp-config.php
@@ -99,7 +99,7 @@ define( 'WP_DEBUG_LOG', true );
 @ini_set( 'display_errors', 1 );
 }
 
-define( 'DISALLOW_FILE_EDIT', false );
+define( 'DISALLOW_FILE_EDIT', false ); 
 define( 'WP_MEMORY_LIMIT', '256M' );
 /* That's all, stop editing! Happy publishing. */
 
