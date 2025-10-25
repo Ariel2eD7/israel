f9e87a675357df3cfd8ab57c0cbccd00c38186b8
Ariel Sabag <relhightech@gmail.com>
2025-10-26 00:27:30 +0300

    s

--- wp-content/plugins/s/S.php
+++ wp-content/plugins/s/S.php
@@ -11,8 +11,17 @@ if (!defined('ABSPATH')) exit;
 // Enqueue styles and scripts
 function s_enqueue_assets() {
     wp_enqueue_style('s-style', plugin_dir_url(__FILE__) . 'style.css');
-    wp_add_inline_script('s-script', "jQuery(document).ready(function($){ $('.s-toggle').click(function(){ $(this).next('.s-content').slideToggle(); }); });");
-    wp_enqueue_script('s-script', '', array('jquery'), false, true); // empty src because we use inline JS
+    
+    // Inline JS for toggling sections
+    $inline_js = <<<JS
+jQuery(document).ready(function($) {
+    $('.s-toggle').click(function() {
+        $(this).next('.s-content').slideToggle();
+    });
+});
+JS;
+
+    wp_add_inline_script('jquery', $inline_js); // Attach to jQuery
 }
 add_action('wp_enqueue_scripts', 's_enqueue_assets');
 
@@ -36,7 +45,7 @@ function s_display_siddur() {
 
         $output .= '<div class="s-section">';
         $output .= "<button class='s-toggle'>{$title}</button>";
-        $output .= "<div class='s-content' style='display:block;'>";  <!-- <-- make it open by default -->
+        $output .= "<div class='s-content' style='display:block;'>"; // open by default
         $output .= "<p dir='rtl' class='hebrew'>{$text}</p>";
 
         if ($audio) {
